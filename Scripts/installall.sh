0e6ff3ea50d38c5bd2a2ea1a5b3338299fa6a5c7
Edoardo Benedetti <edo.benedetti01@gmail.com>
2022-09-13 15:14:44 +0200

    and so the crazy refactoring process sees the sunlight after some months in the dark!

--- Scripts/installall.sh
+++ Scripts/installall.sh
@@ -1,7 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-sudo pacman -S alacritty android-tools base-devel blueman bluez file-roller firefox flameshot flatpak gedit gimp git github-cli helvum kdeconnect neofetch networkmanager obs-studio opendoas pavucontrol pipewire pipewire-alsa pipewire-jack pipewire-pulse pipewire-v4l2 polkit qpwgraph sudo sway thunar wine wireplumber xdg-desktop-portal xdg-desktop-portal-wlr xorg-xwayland zsh
+if [ "$(whoami)" = "root" ]
+then
+	echo "This script should not be executed as the root user."
+	exit
+fi
 
-$HOME/.dotfiles/bin/paru -S azote bottles dropbox hyprland-git paru-bin waybar-hyprland-git zsh-completions
+sudo pacman -S alacritty android-tools base-devel blueman bluez file-roller firefox flameshot flatpak gedit gimp git github-cli helvum kdeconnect lib32-mesa mesa neofetch networkmanager obs-studio opendoas pavucontrol pipewire pipewire-alsa pipewire-jack pipewire-pulse pipewire-v4l2 polkit qpwgraph sudo sway thunar vulkan-radeon wine wireplumber xdg-desktop-portal xdg-desktop-portal-wlr xorg-xwayland zsh
 
-flatpak install authy bitwarden discord spotify
+$HOME/.dotfiles/bin/paru -S authy azote bottles dropbox hyprland-git paru-bin waybar-hyprland-git zsh-completions
+
+flatpak install bitwarden discord spotify
